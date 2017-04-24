95ca5787134a4952d0419a5aaa4cc8eb4ec747db
shsssc <1005308775@qq.com>
2017-04-24 16:01:26 -0700

    Add the WYGIWYS test

--- sample/js/index.js
+++ sample/js/index.js
@@ -1,30 +1,20 @@
 function replaceAll(str, match, rep) {
-    //var target = str;
-    //return target.replace(new RegExp(match, 'g'), rep);
-
-
-     tmparr = str.split(match);
-     result = "";
-     i = 0;
-     for (; i < tmparr.length - 1; i++) {
-     result += tmparr[i] + rep;
-     }
-     result += tmparr[i];
-     return result;
+    var target = str;
+    return target.replace(new RegExp(match, 'g'), rep);
 }
 
 function HTML2raw(source) {
     source = replaceAll(source, "<div></div>", "\n");
     //source = replaceAll(source, "</", "</");
     source = replaceAll(source, "</div>", "");
-    source = replaceAll(source, "<div>", "\n")
+    source = replaceAll(source, "<div>", "\n");
     source = replaceAll(source, "<br>", "\n");
     //var parser = new DOMParser;
     //source=$(parser.parseFromString('<!doctype html><body>' +source,'text/html').body);
     source = _.unescape(source);
     //source = replaceAll(source, "&gt;", ">");
     //source = replaceAll(source, "&amp;", "&");
-    source = replaceAll(source, "&nbsp;", " ");
+    //source = replaceAll(source, "&nbsp;", " ");
     return source;
 }
 
@@ -44,7 +34,6 @@ function drawSequenceDiagram(str) {
         theme: 'simple'
     };
     c.drawSVG(tmp, options);
-    //c.drawSVG(document.querySelector("#tmp");
     var result = tmp.innerHTML;
     tmp.innerHTML = "";
     return result;
@@ -91,10 +80,40 @@ function mdinit() {
     return md;
 }
 
-//当前节点前后第一个 *a*bccccccccc
-//父节点如果不是span，前后
-//当前节点如果是markup
-//
+function bindCusorListener() {
+    function setChangeListener(div, listener) {
+        div.addEventListener("blur", listener);
+        div.addEventListener("keyup", listener);
+        div.addEventListener("paste", listener);
+        div.addEventListener("copy", listener);
+        div.addEventListener("cut", listener);
+        div.addEventListener("delete", listener);
+        div.addEventListener("mouseup", listener);
+
+    }
+    function updateArea(r){
+        var rhtml = r.html();
+        var rtext = rhtml.replace(/<[^<>]+>/g, '');
+        var rendered = md.render(rtext);
+        var renderedWithoutClass = rendered.replace(/[ ]*class=\"[^\"]*\"[ ]*/g, '');
+        var rhtmlWithoutNbspAndClass = rhtml.replace(/[ ]*class=\"[^\"]*\"[ ]*/g, '').replace(/&nbsp;/g, '\xa0');
+        if (renderedWithoutClass=== rhtmlWithoutNbspAndClass) {
+            console.log("don't need be rebuilt");
+        } else {
+            console.log("need to be rebuilt");
+            var caretIndex = getCaretCharacterOffsetWithin(r[0]);
+            r.html(rendered);
+            setCaretOffset(r, 0, caretIndex);
+        }
+    }
+    setChangeListener(document.querySelector("#rarea"), function () {
+        var r = $('#rarea');
+        updateArea(r);
+        autoToggleMarkups();
+    });
+}
+
+
 function autoToggleMarkups() {
     var selection;
     if (window.getSelection)
@@ -103,18 +122,19 @@ function autoToggleMarkups() {
         selection = document.selection;
     if (!selection.isCollapsed || selection.rangeCount !== 1) return undefined;
     var aNode = selection.anchorNode;
-
     //parents detection
-    if ($(aNode.parentNode).is("#sarea")||$(aNode.parentNode).parents("#sarea").length>0 ){return 0;}
-
+    if ($(aNode.parentNode).is("#sarea") || $(aNode.parentNode).parents("#sarea").length > 0) {
+        return 0;
+    }
 
     //inside
     $(".verse-ink-selected").removeClass("verse-ink-selected");
     $(aNode.parentNode).addClass("verse-ink-selected");
     $(".markdown-markup-show").not(".verse-ink-selected").removeClass("markdown-markup-show");
-
     if ($(".verse-ink-selected").hasClass("markdown-markup")) {
-        var classes = $(".verse-ink-selected")[0].classList;
+        var selected= $(".verse-ink-selected");
+        selected.addClass("markdown-markup-show");
+        var classes = selected[0].classList;
         for (i = 0; i < classes.length; i++) {
             if (classes[i].search('_open') !== -1) {
                 var tmpclass = classes[i];
@@ -132,13 +152,16 @@ function autoToggleMarkups() {
             }
         }
     }
-
-
+    
     //previous
     if (selection.anchorOffset === 0) {
         if ($(aNode.parentNode).prev().hasClass("markdown-markup")) {
             $(aNode.parentNode).prev().addClass("markdown-markup-show");
         }
+    }
+
+    if (selection.anchorOffset === 1) {
+        $(aNode.parentElement).prev(".escapeBackslash").addClass('markdown-markup-show');
     }
 
     //next
@@ -161,13 +184,73 @@ function autoToggleMarkups() {
     }
 
     //child
-    $(aNode.parentElement).parents("strong , code , em, u").each(function(){
+    $(aNode.parentElement).parents("strong , code , em, u").each(function () {
         $(this).next().addClass("markdown-markup-show");
         $(this).prev().addClass("markdown-markup-show");
     });
     return true;
 }
 
+function getCaretCharacterOffsetWithin(element) {
+    var caretOffset = 0;
+    var doc = element.ownerDocument || element.document;
+    var win = doc.defaultView || doc.parentWindow;
+    var sel;
+    if (typeof win.getSelection != "undefined") {
+        sel = win.getSelection();
+        if (sel.rangeCount > 0) {
+            var range = win.getSelection().getRangeAt(0);
+            var preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            caretOffset = preCaretRange.toString().length;
+            console.log(':' + caretOffset);
+        }
+    } else if ((sel = doc.selection) && sel.type != "Control") {
+        var textRange = sel.createRange();
+        var preCaretTextRange = doc.body.createTextRange();
+        preCaretTextRange.moveToElementText(element);
+        preCaretTextRange.setEndPoint("EndToEnd", textRange);
+        caretOffset = preCaretTextRange.text.length;
+    }
+    return caretOffset;
+}
+
+function setCaretOffset(currentObj, currentOffset, targetOffset) {
+
+    if (targetOffset <= currentOffset + currentObj.text().length) {//inside this element
+
+        if (currentObj.contents().length === 0) {//this is a text node
+            //setCaretPosition(currentObj[0],targetOffset-currentOffset);
+            var range = document.createRange();
+            var sel = window.getSelection();
+            range.setStart(currentObj[0], targetOffset - currentOffset);
+            range.collapse(true);
+            sel.removeAllRanges();
+            sel.addRange(range);
+            console.log(targetOffset - currentOffset);
+            return -1;
+        }
+        else {//this contains childs
+            var tmpadd = 0;
+            var offset = currentOffset;
+            for (i = 0; i < currentObj.contents().length; i++) {// recursively trying all the childs
+                tmpadd = setCaretOffset($(currentObj.contents()[i]), offset, targetOffset);
+                if (tmpadd < 0) {// found the node
+                    return -1;
+                }
+                else {//not in this one
+                    offset += tmpadd;
+                }
+
+            }
+            console.log("you are in the Neverland");//show never be executed
+        }
+    }
+    else {//the caret should be in some element after this one
+        return currentObj.text().length;
+    }
+}
 
 $("#sarea").bind("DOMNodeInserted DOMNodeRemoved DOMCharacterDataModified", function () {
     if (this.locked === 1) {
@@ -184,6 +267,7 @@ $("#sarea").bind("DOMNodeInserted DOMNodeRemoved DOMCharacterDataModified", func
 
 $(document).ready(function () {
     window.md = mdinit();
+    bindCusorListener();
     var sourceArea = document.querySelector("#sarea");
     sourceArea.addEventListener("paste", function (e) {
         e.preventDefault();
@@ -194,9 +278,6 @@ $(document).ready(function () {
         document.execCommand("insertHTML", false, replaceAll(temp.textContent, "\n", "<br>"));
         sourceArea.locked = 0;
         $(sourceArea).trigger("DOMNodeInserted");
-        tmp.innerHTML = "";
         //console.log("1\n");
     });
 });
-
-setInterval(autoToggleMarkups, 300);
